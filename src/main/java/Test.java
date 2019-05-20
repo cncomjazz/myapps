0f12b444f2cd8e21bf5ec9b80558f0ae5b50ea6b
cncomjazz <20996669@qq.com>
2019-05-20 16:10:30 +0800

    ea02

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,6 +2,7 @@ public class Test {
     public static void hello(){
         System.out.println("hello02");
         System.out.println("commit form jazz01 eclipse");
+        System.out.println("commit form xu02 ea");
     }
 
     //revert1
