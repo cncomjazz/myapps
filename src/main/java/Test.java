02ac49b90a2d25bc943b3287d0fb1445768f53e6
cncomjazz <20996669@qq.com>
2019-05-20 16:35:54 +0800

    ea04

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,6 +4,7 @@ public class Test {
         System.out.println("commit form jazz01 eclipse");
         System.out.println("commit form xu02 ea");
         System.out.println("commit form jazz03 eclipse");
+        System.out.println("commit form xu04");
     }
 
     //revert1
