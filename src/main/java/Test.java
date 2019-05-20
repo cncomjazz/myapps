7b5a49de14ffcfac26d1311958f105304cf9c2cc
cncomjazz <20996669@qq.com>
2019-05-20 17:09:16 +0800

    develop00

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,18 +1,8 @@
 public class Test {
     public static void hello(){
-        System.out.println("hello02");
-        System.out.println("commit form jazz01 eclipse");
-        System.out.println("commit form xu02 ea");
-        System.out.println("commit form jazz03 eclipse");
-        System.out.println("commit form xu04");
-        System.out.println("commit form jazz05 eclipse");
-        System.out.println("commit form xu06");
+        System.out.println("develop00 ");
+
     }
 
-    //revert1
-
-    //revert2
-
-    //revert2
 
 }
